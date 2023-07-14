987c31fc9c6a75bfda36963654fad1b3eecff44d
Nathaniell33 <onevaldosihotang33@gmail.com>
2023-07-14 09:17:22 +0700

    frontend commit

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -14,29 +14,29 @@
   <!-- ***** Header Area Start ***** -->
   <header class="header-area header-sticky">
     <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <nav class="main-nav">
-                    <!-- ***** Logo Start ***** -->
-                    <a href="index.html" class="logo">
-                        <img src="assets/images/logo.png" alt="">
-                    </a>
-                    <!-- ***** Logo End ***** -->
-                    <!-- ***** Menu Start ***** -->
-                    <ul class="nav">
-                        <li><a href="/" class=" {{ ($title === "Home") ? 'active' : '' }} ">Home</a></li>
-                        <li><a href="/about" class=" {{ ($title === "About") ? 'active' : '' }} ">About</a></li>
-                        <li><a href="/deals" class=" {{ ($title === "Deals") ? 'active' : '' }} ">Deals</a></li>
-                        <li><a href="/reservation" class=" {{ ($title === "Reservation") ? 'active' : '' }} ">Reservation</a></li>
-                        <li><a href="/reservation">Book Yours</a></li>
-                    </ul>   
-                    <a class='menu-trigger'>
-                        <span>Menu</span>
-                    </a>
-                    <!-- ***** Menu End ***** -->
-                </nav>
-            </div>
+      <div class="row">
+        <div class="col-12">
+          <nav class="main-nav">
+            <!-- ***** Logo Start ***** -->
+            <a href="index.html" class="logo">
+              <img class="m-auto" style="width:100px; padding-top:15px" src="assets/images/logo.png" alt="">
+            </a>
+            <!-- ***** Logo End ***** -->
+            <!-- ***** Menu Start ***** -->
+            <ul class="nav">
+              <li><a href="/" class=" {{ ($title === "Home") ? 'active' : '' }} ">Home</a></li>
+              <li><a href="/about" class=" {{ ($title === "About") ? 'active' : '' }} ">About</a></li>
+              <li><a href="/deals" class=" {{ ($title === "Deals") ? 'active' : '' }} ">Deals</a></li>
+              <li><a href="/reservation" class=" {{ ($title === "Reservation") ? 'active' : '' }} ">Reservation</a></li>
+              <li><a href="/reservation" class="">Book Yours</a></li>
+            </ul>
+            <a class='menu-trigger'>
+              <span>Menu</span>
+            </a>
+            <!-- ***** Menu End ***** -->
+          </nav>
         </div>
+      </div>
     </div>
   </header>
   <!-- ***** Header Area End ***** -->
